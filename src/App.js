4d085dcccd640b51a1af79045ae042f599704f7f
Time <sukthawat@Mcdonalds-Air.local>
2025-02-18 21:42:09 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import './index.css'; // Global styles
 
-function App() {
+const ComingSoon = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="coming-soon-container d-flex justify-content-center align-items-center min-vh-100">
+      <div className="content-box text-center">
+        {/* Profile Image */}
+        <div className="profile-container">
+        <img src="/IMG_4878.jpg" alt="Your Name" className="img-fluid rounded-circle profile-img" />
+
+        </div>
+
+        {/* Big Coming Soon Text with Animated Underline */}
+        <h1 className="display-1 fw-bold text-uppercase">Coming Soon</h1>
+        <div className="underline"></div>
+
+        {/* Name and Bio */}
+        <h2 className="mt-3 fw-semibold">Time Pattavarakorn</h2>
+        <p className="lead">Chiang Mai University Demonstration School</p>
+
+        {/* Instagram Handle */}
+        <div className="social-links mt-4">
+          <a href="https://www.instagram.com/tpzzzzzz._" className="social-icon">
+            <i className="fab fa-instagram"></i>
+            <span className="ms-2">tpzzzzzz._</span>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default ComingSoon;
